d1427ae3156a39b243a009d0eeecc0e34de78f88
LujanSolo <lujansolo@proton.me>
2024-01-20 22:17:07 -0800

    added handleSeach and state variables to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import SearchForm from './components/SearchForm';
+import ConnectionResult from './components/ConnectionResult';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [connectionResult, setConnectionResult] = useState("");
+
+  const handleSearch = (name1, name2) => {
+    setConnectionResult(`Connection between ${name1} and ${name2} is: `);
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Six Degrees of Film and Television</h1>
+      <SearchForm onSearch={handleSearch} />
+      <ConnectionResult connectionResult={connectionResult} />
+      
     </>
-  )
+  );
 }
 
-export default App
+export default App;
