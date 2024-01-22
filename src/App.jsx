f514afd09a834da35971e6b333f43f179ef92ce6
LujanSolo <lujansolo@proton.me>
2024-01-21 23:11:15 -0800

    updated app.js with new api functionality

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,9 @@ const TMDB_API_KEY = import.meta.env.VITE_TMDB_API_KEY;
 function App() {
   const [connectionResult, setConnectionResult] = useState("");
 
-  const getActorId = async(name_ => {
+  const getActorId = async (name) => {
     try {
-      const response = await axios.get(`https://api.themoviedb.org/3/search/person?api_key=${TMDB_API_KEY}&query=${name_}`);
+      const response = await axios.get(`https://api.themoviedb.org/3/search/person?api_key=${TMDB_API_KEY}&query=${name}`);
 
       const actor = response.data.results.find((result) => result.known_for_department === 'Acting');
       return actor ? actor.id : null;
@@ -18,7 +18,7 @@ function App() {
       console.error("Error fetching actor ID", error.message);
       throw error;
     }
-  })
+  };
 
   const getActorFilmography = async (actorId) => {
     try {
@@ -37,13 +37,22 @@ function App() {
   };
 
   const handleSearch = async (name1, name2) => {
-    try{
+    try {
       const actorId1 = await getActorId(name1);
       const actorId2 = await getActorId(name2);
 
       if (!actorId1 || !actorId2) {
         throw new Error('Actor not found. Please check the names and try again.');
       }
+
+      const filmography1 = await getActorFilmography(actorId1);
+      const filmography2 = await getActorFilmography(actorId2);
+
+      const result = `The connection between ${name1} and ${name2} is ${getDegreesOfSeparation(filmography1, filmography2)}`;
+      setConnectionResult(result);
+    } catch (error) {
+      console.error('Error in handleSearch', error.message);
+      setConnectionResult('Error finding connection. Please try again');
     }
   }
   return (
