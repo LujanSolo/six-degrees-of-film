e345442f78ed6fbeb45aebd8b18752ef6eabf918
LujanSolo <lujansolo@proton.me>
2024-02-01 13:19:28 -0800

    initial commit of existing vsCode-base

--- src/App.jsx
+++ src/App.jsx
@@ -3,34 +3,25 @@ import { useState } from 'react';
 function App() {
   const [isLoading, setIsLoading] = useState(false);
 
-  const fetchDataFromApi = async () => {
+  const fetchDataFromTMDB = async () => {
     setIsLoading(true);
-    const actorName = document.getElementById('keyword').value;
-    const encodedActorName = encodeURIComponent(actorName);
-  
-    const url = `https://moviesdatabase.p.rapidapi.com/titles/search/keyword/${encodedActorName}`;
-    const options = {
-      method: 'GET',
-      headers: {
-        'X-RapidAPI-Key': '40d5e8b600msh8c096a9cc653e9dp182fb0jsn39cf6a8a9916',
-        'X-RapidAPI-Host': 'moviesdatabase.p.rapidapi.com',
-      },
-    };
-  
-    let response; // Declare the variable outside the try block
-  
+
+    const tmdbApiKey = import.meta.env.VITE_TMDB_API_KEY;
+    const personName = "Judi Dench";
+
+    const url = `https://api.themoviedb.org/3/search/person?api_key=${tmdbApiKey}&query=${personName}`;
+
     try {
-      response = await fetch(url, options);
-  
+      const response = await fetch(url);
+
       if (!response.ok) {
-        throw new Error(`Error: ${response.status} - ${response.statusText}`);
+        throw new Error(`Error: ${response.status} = ${response.statusText}`);
       }
-  
+
       const result = await response.json();
       console.log('Result:', result);
     } catch (error) {
-      console.error('Error fetching data from API:', error.message);
-      console.log('Response:', response ? await response.text() : 'Undefined response'); // Log the response for additional details
+      console.error('Error fetching data from TMDB:', error.message);
     } finally {
       setIsLoading(false);
     }
@@ -38,10 +29,8 @@ function App() {
 
   return (
     <>
-      <h1>MoviesDatabase App</h1>
-      <label htmlFor="keyword">Enter an Actor's Name</label>
-      <input id="keyword" type="text" placeholder="Enter a name..." />
-      <button onClick={() => fetchDataFromApi()}>Fetch Data</button>
+      <h1>6 DEGREES OF FILM</h1>
+      <button onClick={fetchDataFromTMDB}>Fetch TMDB Data</button>
       {isLoading && <p>Loading...</p>}
     </>
   );
